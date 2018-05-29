7364575fa16fc4d3fd946001f2dfd711ac0fb42f
Kathryn Brusewitz <kathryn.brusewitz@gmail.com>
2018-05-29 08:23:36 -0700

    update port

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+// var port = process.env.PORT || 3000;
+var port = 8080;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
