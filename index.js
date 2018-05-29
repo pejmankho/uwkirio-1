bf2ac0a07ccbf4ed420b131fca55bfedde068fa5
Kathryn Brusewitz <kathryn.brusewitz@gmail.com>
2018-05-29 02:32:00 -0700

    wrong files

---
+++ index.js
@@ -0,0 +1,21 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('pressure', function(msg){
+    io.emit('pressure', msg);
+  });
+  socket.on('humidity', function(msg){
+    io.emit('humidity', msg);
+  });
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
